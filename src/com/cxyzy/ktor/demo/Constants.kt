1a6ac06cdc57ee5b778d09244e6f56a974b33de5
Abhishek Dubey <abhishekdubey331@gmail.com>
2020-02-01 14:51:27 +0530

    added random memes

--- src/com/cxyzy/ktor/demo/Constants.kt
+++ src/com/cxyzy/ktor/demo/Constants.kt
@@ -5,3 +5,5 @@ const val dbName = "memes_app"
 const val collectionName = "memes"
 
 const val PAGE_SIZE = 100
+
+const val randomCat = "sarcasm"
--- src/com/cxyzy/ktor/demo/controller/MemeController.kt
+++ src/com/cxyzy/ktor/demo/controller/MemeController.kt
@@ -4,6 +4,7 @@ import com.cxyzy.ktor.demo.PAGE_SIZE
 import com.cxyzy.ktor.demo.bean.Meme
 import com.cxyzy.ktor.demo.collectionName
 import com.cxyzy.ktor.demo.dbName
+import com.cxyzy.ktor.demo.randomCat
 import com.cxyzy.ktor.demo.utils.ApiUrls
 import com.cxyzy.ktor.demo.utils.MemeUtil
 import io.ktor.application.call
@@ -13,8 +14,9 @@ import io.ktor.routing.Route
 import io.ktor.routing.get
 import io.ktor.routing.route
 import org.koin.ktor.ext.inject
+import org.litote.kmongo.*
 import org.litote.kmongo.coroutine.CoroutineClient
-import org.litote.kmongo.lt
+import org.litote.kmongo.coroutine.aggregate
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
@@ -40,6 +42,29 @@ fun Route.userRoutes() {
             }
         }
     }
+
+
+    route(ApiUrls.FETCH_RANDOM_MEMES) {
+        get {
+            val memes = client.getDatabase(dbName)
+                .getCollection<Meme>(collectionName)
+                .aggregate<Meme>(
+                    match(
+                        Meme::category eq randomCat
+                    ),
+                    sample(PAGE_SIZE),
+                    sort(memeUtil.sortDescending())
+                ).toList()
+
+            if (memes.isNullOrEmpty()) {
+                memeUtil.handleEmptyData(call)
+            } else {
+                call.respond(HttpStatusCode.OK, memes)
+            }
+        }
+    }
+
+
 }
 
 
